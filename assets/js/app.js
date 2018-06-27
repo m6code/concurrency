47226a96197d67ed542464830e623f10166571dc
Benjamin M6 <benjamynfrank101@gmail.com>
2018-06-27 12:13:45 +0100

    Update UI with data from api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,36 @@
 
 // instantiate EasyHttp
-const httpRequest = new EasyHTTP();
-
-// Instantiate the UI class
-const ui = new UI;
-
-// create the query string
-const query = `${ui.fromCurrency}_${ui.toCurrency}`;
+const httpRequest = new EasyHTTP;
 
 // Fetch data from API
 document.addEventListener('DOMContentLoaded', convertCurrency());
+// convert button
+document.getElementById('convertBtn')
+    .addEventListener('click', convertCurrency);
 
 // Convert Currency
 function convertCurrency() {
+    // UI variables
+    const fromCurrency = document.getElementById('CURR_FR').value;
+    const toCurrency = document.getElementById('CURR_TO').value;
+    const fromCurrencyValue = document.getElementById('curr-from-value').value;
+    const toCurrencyValue = document.getElementById('curr-to-value');
+    // create the query string
+    const query = `${fromCurrency}_${toCurrency}`;
+
     // make http request
     httpRequest.get(`https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`)
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data);
+            // Update the UI with data
+            paint(data);
+        })
         .catch(err => console.log(err));
+
+    // Update UI
+    function paint(data) {
+        // toCurrencyValue.value = data.query;
+        toCurrencyValue.value = Math.round(data.USD_NGN * fromCurrencyValue);
+        console.log(toCurrencyValue.value);
+    }
 }
--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,9 +1,5 @@
 class UI {
-    constructor(){
-        this.fromCurrency = document.getElementById('CURR_FR').value;
-        this.toCurrency = document.getElementById('CURR_TO').value;
-        this.fromCurrencyValue = document.getElementById('curr-from-value').value;
-        this.toCurrencyValue = document.getElementById('curr-to-value');
-        this.convertBtn = document.getElementById('convertBtn');
+    constructor() {
+
     }
 }
